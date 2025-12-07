37828f300cb6066659dfe323ce4295faf04f610a
FrederickHuangLin <huanglinfrederick@gmail.com>
2025-12-07 16:50:35 -0500

    official 0.0.2 version to pypi

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from setuptools import find_packages, setup
 
 classes = """
@@ -12,26 +13,34 @@ classifiers = [s.strip() for s in classes.split('\n') if s]
 
 description = ('Metabolomics Variational Autoencoders.')
 
+# Read README with explicit encoding (avoids Unicode issues on Windows)
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text(encoding="utf-8")
 
 setup(name='metvae',
       version='0.0.2',
       description=description,
       long_description=open("README.md").read(),
       long_description_content_type="text/markdown",
+      author="Huang Lin",
       author_email="huanglinfrederick@gmail.com",
+      maintainer="Huang Lin",
       maintainer_email="huanglinfrederick@gmail.com",
+      url="https://github.com/FrederickHuangLin/MetVAE-PyPI",
+      license="MIT",
+      license_files=["LICENSE"],
       packages=find_packages(exclude=['tests', 'tests.*']),  # Excludes tests
       install_requires=[
           'joblib>=1.2.0',
           'numpy>=1.20.0',
           'scipy',
           'statsmodels',
-          'scikit-learn',
           'tensorboard',
           'tqdm',
           'pandas>=1.0.0',
           'torch>=1.8.0',
-          'pytorch-lightning>=1.3.1'
+          'pytorch-lightning>=1.3.1',
+          "networkx>=2.6"
       ],
       classifiers=classifiers,
       python_requires=">=3.9",
